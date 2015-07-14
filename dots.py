acd4d75202bfc0d261f7ce0a43058075f9034492
David Eads <davideads@gmail.com>
2015-07-14 14:17:57 -0400

    update to use decennial census

--- dots.py
+++ dots.py
@@ -9,8 +9,13 @@ db = dataset.connect(POSTGRES_URL)
 census_table = db['census_data']
 
 def get_data(feature):
-    import ipdb; ipdb.set_trace();
-    pass
+    result = census_table.find_one(geo_id=feature.geo_id)
+    return {
+        'white': int(result['d003']),
+        'black': int(result['d004']),
+        'asian': int(result['d006']),
+        #'hispanic': result['d010'],
+    }
 
 def make_dots():
     args = [
@@ -23,6 +28,7 @@ def make_dots():
         25
     ]
     dots = DotDensityPlotter(*args)
+    dots.plot()
 
 if __name__ == '__main__':
     make_dots()
--- import.py
+++ import.py
@@ -6,7 +6,8 @@ from slugify import slugify
 from collections import OrderedDict
 
 INPUT_FILES = (
-    ('acs-2013', 'data/acs2013_5yr_B02001_14000US22071014400/acs2013_5yr_B02001_14000US22071014400.csv'),
+    #('acs-2013', 'data/acs2013_5yr_B02001_14000US22071014400/acs2013_5yr_B02001_14000US22071014400.csv'),
+    ('decennial-2010', 'data/decennial/DEC_10_SF1_P5_with_ann.csv'),
 )
 
 POSTGRES_URL = 'postgresql:///nola_demographics'
@@ -21,16 +22,24 @@ def import_data(db, product, filename):
         rows = list(csv.reader(f))
 
     raw_columns = rows.pop(0)
+    rows.pop(0)
+
     columns = [slugify(column, separator='_') for column in raw_columns]
+    #for column in raw_columns:
+        #processed_name = .replace('some_other_race', 'other')
+        #name_parts = processed_name.split('_')
+        #if len(name_parts) > 1:
+            #processed_name = '_'.join(name_parts[1:])
+        #columns.append(processed_name)
+
+    table = db['census_data']
 
     data = []
     for i, row in enumerate(rows):
         processed_row = OrderedDict(zip(columns, row))
         processed_row['product'] = product
-        processed_row['geographic_id'] = processed_row['geoid']
-        del processed_row['geoid']
+        data.append(processed_row)
 
-    table = db['census_data']
     table.insert_many(data)
 
 
