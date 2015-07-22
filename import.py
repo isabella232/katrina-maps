db92792eedb088158d0ff0466a7a83b5a72a85c1
David Eads <davideads@gmail.com>
2015-07-22 13:41:21 -0400

    parse and add all races from population estimates to db

--- import.py
+++ import.py
@@ -59,26 +59,52 @@ def import_fips():
 
     table.insert_many(data)
 
-def import_2000_population_estimates():
+def _write_2000_population_estimate(race, row):
     table = db['population_estimates']
 
+    print 'processing %s (%s)' % (row['CTYNAME'], race)
+
+    for year in range(2000, 2011):
+        estimate_key = 'POPESTIMATE%s' % year
+
+        data = {
+            'county': row['CTYNAME'],
+            'year': year,
+        }
+        data[race] = row[estimate_key]
+
+        table.upsert(data, ['year', 'county'])
+
+
+def import_2000_population_estimates():
     with open(ESTIMATES_2000_FILE) as f:
         rows = list(csv.DictReader(f))
 
     for row in rows:
-        if (row['STNAME'] == 'Louisiana' 
-                and row['CTYNAME'] in METRO_PARISHES 
+        if (row['STNAME'] == 'Louisiana'
+                and row['CTYNAME'] in METRO_PARISHES
                 and row['SEX'] == '0'):
 
             if row['ORIGIN'] == '2' and row['RACE'] == '0':
-                print 'found hispanic row for %s' % row['CTYNAME']
-                print row['POPESTIMATE2010']   
+                _write_2000_population_estimate('hispanic', row)
 
             if row['ORIGIN'] == '1' and row['RACE'] == '1':
-                print 'found white row for %s' % row['CTYNAME']
-                print row['POPESTIMATE2010']  
-                
-           
+                _write_2000_population_estimate('white', row)
+
+            if row['ORIGIN'] == '1' and row['RACE'] == '2':
+                _write_2000_population_estimate('black', row)
+
+            if row['ORIGIN'] == '1' and row['RACE'] == '3':
+                _write_2000_population_estimate('american_indian', row)
+
+            if row['ORIGIN'] == '1' and row['RACE'] == '4':
+                _write_2000_population_estimate('asian', row)
+
+            if row['ORIGIN'] == '1' and row['RACE'] == '5':
+                _write_2000_population_estimate('native_hawaiian', row)
+
+            if row['ORIGIN'] == '1' and row['RACE'] == '6':
+                _write_2000_population_estimate('two_or_more', row)
 
 
 if __name__ == '__main__':
