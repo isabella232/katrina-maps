3fe1edf1912713e2825ab24429bdfb441dcb919f
David Eads <davideads@gmail.com>
2015-07-31 00:58:54 -0400

    don't remote output dir, add some notes

--- process.sh
+++ process.sh
@@ -21,10 +21,7 @@ psql nola_demographics -c "SELECT postgis_full_version()"
 # mapshaper data/Parishes_LDOTD_2007/Parishes_LDOTD_2007.shp -erase data/lousiana-water/tiger_la_water_CENSUS_2006.shp -simplify 30% -o data/Parishes_LDOTD_2007-clean/Parishes_LDOTD_2007-clean.shp
 
 
-rm -Rf ./output/*
-
-echo "Import census data"
-./import.py
+#rm -Rf ./output/*
 
 echo "Import 2000 census block groups"
 PGCLIENTENCODING=LATIN1 ogr2ogr -f PostgreSQL PG:dbname=nola_demographics data/bg22_d00_shp-clean/bg22_d00_shp-clean.shp -s_srs EPSG:4269  -t_srs EPSG:4269 -nlt multipolygon -nln block_groups_2000
@@ -32,6 +29,14 @@ PGCLIENTENCODING=LATIN1 ogr2ogr -f PostgreSQL PG:dbname=nola_demographics data/b
 echo "Import 2013 census block groups"
 PGCLIENTENCODING=LATIN1 ogr2ogr -f PostgreSQL PG:dbname=nola_demographics data/cb_2013_22_bg_500k-clean/cb_2013_22_bg_500k-clean.shp  -t_srs EPSG:4269 -nlt multipolygon -nln block_groups_2013
 
+echo "Import census data"
+./import.py
+
+psql nola_demographics -c "create or replace view census_2000_race as 
+select bg.wkb_geometry, bg.geoid2, d.vd05 as white, d.vd06 as black, d.vd08 as asian, d.vd02 as hispanic
+from block_groups_2000 bg
+join census_data d on bg.geoid2 = d.geo_id2
+where d.product='decennial-2000-bg';"
 
 echo "Generate summary files"
 ./summarize.py
